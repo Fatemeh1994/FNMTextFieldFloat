59bdbb9e4a70ba965501184c28d702b30516cc2e
Reza Khonsari <rezakhonsari@icloud.com>
2021-08-29 18:12:55 +0430

    fix: errorlabel constraint

--- Sources/FNMTextFieldFloat/FNMTextFieldFloatView.swift
+++ Sources/FNMTextFieldFloat/FNMTextFieldFloatView.swift
@@ -233,6 +233,7 @@ open class FNMTextFieldFloatView: UIView {
             errorLabel.translatesAutoresizingMaskIntoConstraints = false
             let errorLabelPointSize = errorLabel.font.pointSize
             NSLayoutConstraint.activate([
+                errorLabel.topAnchor.constraint(equalTo: textFieldFloat.bottomAnchor, constant: 4),
                 errorLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
                 errorLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
                 errorLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 3 + errorLabelPointSize)
